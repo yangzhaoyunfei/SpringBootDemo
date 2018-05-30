426fc279e25412228a9a2f7d7555ddf8d6da0feb
tangzhongwei <tangzw@zjbdos.com>
2018-05-30 12:29:32 +0800

    comment

--- chapter11/src/main/java/com/dudu/dao/LearnResourceMapper.java
+++ chapter11/src/main/java/com/dudu/dao/LearnResourceMapper.java
@@ -2,10 +2,12 @@ package com.dudu.dao;
 
 import com.dudu.domain.LearnResource;
 import com.dudu.util.MyMapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public interface LearnResourceMapper extends MyMapper<LearnResource> {
     List<LearnResource> queryLearnResouceList(Map<String,Object> map);
 }
--- chapter12/src/test/java/com/dudu/assertThatDemo.java
+++ chapter12/src/test/java/com/dudu/assertThatDemo.java
@@ -15,6 +15,13 @@ import static org.junit.Assert.assertThat;
 
 /**
  * Description: assertThat 基本语法
+ * ##JUnit 4.4 默认提供一些可读的描述性错误信息
+ *     String s = "hello world!";
+ *     assertThat( s, anyOf( containsString("developer"), containsString("Works") ) );
+ *     // 如果出错后，系统会自动抛出以下提示信息：
+ *     java.lang.AssertionError:
+ *     Expected: (a string containing "developer" or a string containing "Works")
+ *     got: "hello world!"
  *
  * @author selfimpr  tangzw@zjbdos.com
  * @date 2018/05/28
@@ -25,6 +32,33 @@ public class assertThatDemo {
     private Object object=new Object();
     private Object testedValue=new Object();
     private Object expectedValue=new Object();
+    private String testedString = "demo";
+    private String expectedString = "demo";
+    private String subString = "demo";
+    private String suffix = "demo";
+    private String prefix = "demo";
+
+    public void common() {
+        /*
+         * assertThat 基本语法
+         * assertThat( [value], [matcher statement] );
+         */
+        String something = "something";
+        /**
+         * 联合匹配符not和equalTo表示“不等于”
+         */
+        assertThat(something, not(equalTo("developer")));
+
+        /**
+         * 联合匹配符not和containsString表示“不包含子字符串”
+         */
+        assertThat(something, not(containsString("Works")));
+
+        /**
+         * 联合匹配符anyOf和containsString表示“包含任何一个子字符串”
+         */
+        assertThat(something, anyOf(containsString("developer"), containsString("Works")));
+    }
 
 
     /**
@@ -91,6 +125,38 @@ public void generic(){
 }
 
     /**
+     * 字符相关匹配符
+     */
+    public void string() {
+
+        /**equalTo匹配符断言被测的testedValue等于expectedValue，
+         * equalTo可以断言数值之间，字符串之间和对象之间是否相等，相当于Object的equals方法
+         */
+        assertThat(testedValue, equalTo(expectedValue));
+
+        /**equalToIgnoringCase匹配符断言被测的字符串testedString
+         *在忽略大小写的情况下等于expectedString
+         */
+        assertThat(testedString, equalToIgnoringCase(expectedString));
+
+        /**equalToIgnoringWhiteSpace匹配符断言被测的字符串testedString
+         *在忽略头尾的任意个空格的情况下等于expectedString，
+         *注意：字符串中的空格不能被忽略
+         */
+        assertThat(testedString, equalToIgnoringWhiteSpace(expectedString));
+
+        /**containsString匹配符断言被测的字符串testedString包含子字符串subString**/
+        assertThat(testedString, containsString(subString));
+
+        /**endsWith匹配符断言被测的字符串testedString以子字符串suffix结尾*/
+        assertThat(testedString, endsWith(suffix));
+
+        /**startsWith匹配符断言被测的字符串testedString以子字符串prefix开始*/
+        assertThat(testedString, startsWith(prefix));
+    }
+
+
+    /**
      * 集合相关匹配符
      */
     public void collection(){
@@ -107,11 +173,14 @@ public void generic(){
 
     /**hasEntry匹配符断言被测的Map对象mapObject含有一个键值为"key"对应元素值为"value"的Entry项*/
     assertThat(mapObject, hasEntry("key", "value" ) );
-    /**hasItem匹配符表明被测的迭代对象iterableObject含有元素element项则测试通过*/
+
+        /**hasItem匹配符表明被测的迭代对象iterableObject含有元素element项则测试通过*/
     assertThat(iterableObject, hasItem (element));
-    /** hasKey匹配符断言被测的Map对象mapObject含有键值“key”*/
+
+        /** hasKey匹配符断言被测的Map对象mapObject含有键值“key”*/
     assertThat(mapObject, hasKey ("key"));
-    /** hasValue匹配符断言被测的Map对象mapObject含有元素值value*/
+
+        /** hasValue匹配符断言被测的Map对象mapObject含有元素值value*/
     assertThat(mapObject, hasValue(value));
 }
 }
